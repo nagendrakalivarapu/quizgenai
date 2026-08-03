2a97381836c95a79bd6e53e598f129a2351aa641
nagendra <nagendrakalivarapu789@gmail.com>
2026-08-04 03:13:27 +0530

    Update backend URL

--- quizgenai-frontend/src/services/api.js
+++ quizgenai-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://quizgenai-backend-pluv.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
