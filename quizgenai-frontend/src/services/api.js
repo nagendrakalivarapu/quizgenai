2d9a08d4d3eb177f6b78540b0ec0de7c2fae1f48
nagendra <nagendrakalivarapu789@gmail.com>
2026-08-04 02:32:13 +0530

    Configure application for deployment

--- quizgenai-frontend/src/services/api.js
+++ quizgenai-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://institute-workshops-beaver-soldiers.trycloudflare.com/api",
+    baseURL: "http://localhost:8080/api",
     headers: {
         "Content-Type": "application/json"
     }
