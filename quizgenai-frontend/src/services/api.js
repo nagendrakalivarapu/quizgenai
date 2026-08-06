a0e831975d21f48464a381237c0d4edf1dd19323
nagendra <nagendrakalivarapu789@gmail.com>
2026-08-06 22:54:17 +0530

    Add JWT interceptor

--- quizgenai-frontend/src/services/api.js
+++ quizgenai-frontend/src/services/api.js
@@ -1,4 +1,3 @@
-// src/api/api.js
 import axios from "axios";
 
 const api = axios.create({
@@ -6,6 +5,18 @@ const api = axios.create({
     headers: {
         "Content-Type": "application/json"
     }
+});
+
+// Add JWT token automatically
+api.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 export default api;
