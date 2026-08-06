1b591589649326c2f5da5812229b565fabd2ba63
nagendra <nagendrakalivarapu789@gmail.com>
2026-08-06 22:32:57 +0530

    Update production backend URL

--- quizgenai-frontend/src/services/api.js
+++ quizgenai-frontend/src/services/api.js
@@ -1,7 +1,8 @@
+// src/api/api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://quizgenai-backend-pluv.onrender.com/api",
+    baseURL: "https://quizgenai-backend-ua85.onrender.com/api",
     headers: {
         "Content-Type": "application/json"
     }
